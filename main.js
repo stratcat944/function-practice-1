575dfa8f4b4afa837e8807afc4226274aa4b362c
Steve Domenico <stratcat944@gmail.com>
2015-06-25 12:31:59 -0600

    updated to Ex. 4

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
+//=========== < Exercise 1  >===========//
 //=========== < TRIPLE FIVE > ===========//
 
-//var tripleFive = function() {
-//	for(i = 0 ; i < 3 ; i++) {
-//		console.log("Five!");
-//	}
-//}
-//
-//tripleFive()
+// var tripleFive = function() {
+// 	for(i = 0 ; i < 3 ; i++) {
+// 		console.log("Five!");
+// 	}
+// }
+// 
+// tripleFive()
 
 
+//=========== < Exercise 2  > /==========//
 //=========== < LAST LETTER > ===========//
-var lastLetter = function(){
 
-	var word = prompt("Enter a single word:"); 
-	var wordLength = word.length
+// var lastLetter = function(){
+// 
+// 	var word = prompt("Enter a single word:"); 
+// 	var wordLength = word.length
+// 
+// 	return word[wordLength-1]
+// 
+// }
+// 
+// console.log(lastLetter());
 
-	return word[wordLength-1]
 
+//=========== < Exercise 3  > ===========//
+//=========== <    Square   > ===========//
+
+//function square(x) {
+//	return x * x;
+//}
+//console.log(square(4));
+//console.log(square(37));
+//console.log(square(9));
+
+
+//=========== < Exercise 4  > ===========//
+//=========== <    Negate   > ===========//
+
+function negate(n) {
+	return n - (n * 2);
 }
 
-console.log(lastLetter());
+console.log(negate(3));
+console.log(negate(27));
+console.log(negate(-7));
+
